e1e4f421b41a4c907f41e342957f190f261ccbb7
zachwillingham <113157767+zachwillingham@users.noreply.github.com>
2023-12-03 15:40:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,155 +1,38 @@
-/*button zero click functionality*/
-document.querySelector("#btn-0").onclick = function(){
+// index.js
 
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "0";
-}
+// Event listener for numeric and operator buttons
+document.querySelectorAll('.btn-num, .btn-other').forEach(button => {
+    button.addEventListener('click', () => {
+        // Append the clicked button value to the input field
+        document.getElementById('equation').value += button.textContent;
+    });
+});
 
-/*button one click functionality*/
-document.querySelector("#btn-1").onclick = function(){
+// Event listener for the CLR (clear) button
+document.getElementById('btn-clr').addEventListener('click', () => {
+    // Clear the input field
+    document.getElementById('equation').value = '';
+});
 
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "1";
-}
+// Event listener for the "=" button
+document.getElementById('btn-eq').addEventListener('click', () => {
+    // Get the equation from the input field
+    const equation = document.getElementById('equation').value;
 
-/*button two click functionality*/
-document.querySelector("#btn-2").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "2";
-}
-
-/*button three click functionality*/
-document.querySelector("#btn-3").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "3";
-}
-
-/*button four click functionality*/
-document.querySelector("#btn-4").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "4";
-}
-
-/*button five click functionality*/
-document.querySelector("#btn-5").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "5";
-}
-
-/*button six click functionality*/
-document.querySelector("#btn-6").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "6";
-}
-
-/*button seven click functionality*/
-document.querySelector("#btn-7").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "7";
-}
-
-/*button eight click functionality*/
-document.querySelector("#btn-8").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "8";
-}
-
-/*button nine click functionality*/
-document.querySelector("#btn-9").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "9";
-}
-
-/*button plus click functionality*/
-document.querySelector("#btn-add").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "+";
-}
-
-/*button minus click functionality*/
-document.querySelector("#btn-sub").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "-";
-}
-
-/*button times click functionality*/
-document.querySelector("#btn-mult").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "×";
-}
-
-/*button divide click functionality*/
-document.querySelector("#btn-div").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "÷";
-}
-
-/*button modulo click functionality*/
-document.querySelector("#btn-mod").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "%";
-}
-
-/*button squared click functionality*/
-document.querySelector("#btn-sqr").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "²";
-}
-
-/*button power click functionality*/
-document.querySelector("#btn-pow").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "^";
-}
-
-/*button decimal click functionality*/
-document.querySelector("#btn-dec").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + ".";
-}
-
-/*button open par click functionality*/
-document.querySelector("#btn-open-par").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "(";
-}
-
-/*button close par click functionality*/
-document.querySelector("#btn-close-par").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + ")";
-}
-
-/*button clear click functionality*/
-document.querySelector("#btn-clr").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = "";
-}
-
-/*button enter click functionality*/
-/* NEEDS TO PASS THE TEXT STRING TO C++ PROGRAM
-document.querySelector("#btn-eq").onclick = function(){
-
-    const textbox = document.querySelector("#textbox");
-    textbox.innerText = textbox.innerText + "0";
-}
-*/
+    // Send the equation to the C++ backend using fetch
+    fetch('/calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ equation: equation }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the result on the calculator display
+        document.getElementById('equation').value = data.result;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
